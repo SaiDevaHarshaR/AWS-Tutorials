52b6c2814980ba0bd8ae515b5fc6b1223bb175f1
R Sai Deva Harsha <110902188+SaiDevaHarshaR@users.noreply.github.com>
2023-07-04 10:42:22 +0530

    Add files via upload

---
+++ S3-Bucket-Tutorial/script.js
@@ -0,0 +1,10 @@
+// JavaScript code can be added here if needed
+
+// Link to AWS S3 service
+var awsS3Link = document.createElement('a');
+awsS3Link.href = 'https://aws.amazon.com/s3/';
+awsS3Link.target = '_blank';
+awsS3Link.textContent = 'Click me to get S3 Documentation!';
+awsS3Link.classList.add('link');
+
+document.body.appendChild(awsS3Link);
